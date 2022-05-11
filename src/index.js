0f64ffd9ffdc2a27219c875070aa943225bfae19
Annaanto <aantonioli@sayit.ch>
2022-05-11 19:45:37 +0200

    initial set up & chart biulding & onclick

--- src/index.js
+++ src/index.js
@@ -1,9 +1,79 @@
-//Here we're importing items we'll need. You can add other imports here.
+import c3 from "c3";
+import d3 from "d3";
+const months=["Gen", "Feb", "Mar", "Apr", "May", "June"];
+;
 
-//The first function. Remove this.
-const btn = document.querySelector("button");
-btn.onclick = function () {
-  alert("You ran some JavaScript");
+window.loadchart = function (json) {
+  //console.log(json);
+  const obj=JSON.parse(json);
+  //console.log(obj);
+
+  //const type=obj.type;
+  //const columns=obj.data;
+  const{type,data,legend,position}=obj;
+
+
+  var chart = c3.generate({
+    size: {
+      width: 1000,
+      height: 800,
+    },
+    bindto: '#chart',
+    axis: {
+      x: {
+        type: "category",
+      },
+    },
+
+    data: {
+      onclick: function (clickedData) { 
+        alert("hi");
+        console.log(clickedData);
+        const {id, index}=clickedData;
+        console.log(id,index);
+        const month=months[index];
+        console.log(id,index,month);
+        const object={id,month};
+        //const obj={id: clickedData.id, month: months[clickedData.index]};
+        
+        console.log(object);
+        FileMaker.PerformScript("Get Data",JSON.stringify(object));
+       },
+      /* selection: {
+        multiple: true,
+        draggable: true,
+      }, */
+      stack: {
+        normalize: true
+      },
+      labels: true,
+      x: "x",
+      type: type,
+      columns: data,
+    },
+
+    legend: {
+      show: legend,
+      position: position,
+    },
+    donut: {
+      title: "Fruits"
+  }
+    // tooltip: {
+    //   format: {
+    //     title: function (d) { return 'Data ' + d; },
+    //     value: d3.format('$')
+    //   //   value: function (value, ratio, id) {
+    //   //     var format =  d3.format('€') ;
+    //   //     return format(value);
+    //   // }
+    //      }
+    //   }
+  
+          
+  });
 };
 
-//
+function sum(a, b) {
+  return a+b;
+};
